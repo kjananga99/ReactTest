6cc953a89bf4e2199673df40b506ae19aa332fe1
jananga <kavinduwijewardhana1999@gmail.com>
2024-06-20 15:38:59 +0530

    update increment and decrement

--- src/App.js
+++ src/App.js
@@ -3,27 +3,34 @@ import './App.css';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import { useState } from 'react';
 
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 
 function App() {
-  let count = 0;
+
+  const [count, setCount] = useState(0);
+
+  // let count = 0;
 
   const increment = () =>{
-    count +=1;
+    // count +=1;
+    setCount(count+1);
   }
 
   const decriment = () =>{
-    count -=1;
+    // count -=1;
+    setCount(count-1);
   }
+
 
   return (
     <>
 
     <span className='title'>My Counter</span>
     <p className='subTitle'> The count is {count}</p>
-    <button className='button'>-</button>
-    <button className='button'>+</button>
+    <button onClick={decriment} className='button'>-</button>
+    <button onClick={increment} className='button'>+</button>
 
     </>
 
